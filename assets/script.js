e2ef104627fda709804dce14ffa91433f350ee8b
Dharkaron <152791804+Dharkaron@users.noreply.github.com>
2024-01-09 20:57:24 +0100

    alternative JS code

--- assets/script.js
+++ assets/script.js
@@ -1,84 +1,72 @@
 const slides = [
 	{
-		"image":"./assets/images/slideshow/slide1.jpg",
+		"image":"slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide2.jpg",
+		"image":"slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide3.jpg",
+		"image":"slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide4.png",
+		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
 
-//Variables Générales
-const bannerImg = document.querySelector(".banner-img")
-const bannerCaption = document.querySelector("#banner p")
-const arrowLeft = document.querySelector(".arrow_left")
-const arrowRight = document.querySelector(".arrow_right")
-const banner = document.getElementById("banner")
-const bannerDots = document.querySelector(".dots")
+// Variables Générales
+const bannerImg = document.querySelector(".banner-img");
+const bannerCaption = document.querySelector("#banner p");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const bannerDots = document.querySelector(".dots");
 
-let index = 0
-
+let currentIndex = 0;
 
 
-// Création et Affichage des bullet points dans la page html
-//
-//
-let slidersList = document.createElement("ul")
-	////remplacement de la div par une liste ul
-	bannerDots.replaceWith(slidersList)
-	slidersList.classList.add("dots")
-		////Ajout de la liste dans le html
-		banner.appendChild(slidersList)
+//Ajout des bulletPoint "dot" dans le html
+for (let i=0; i<slides.length; i++){
+	let dot = document.createElement("div");
+	dot.classList.add("dot");
+	bannerDots.appendChild(dot);
 
-	
-	for (let i=0; i<slides.length; i++){
-		let bulletpoint = document.createElement("li")
-		bulletpoint.classList.add("dot")
-		slidersList.appendChild(bulletpoint)
-
-		if (i == index){
-			bulletpoint.classList.add("dot_selected")
-		}
-	}
-	
+	if (i == currentIndex){
+	dot.classList.add("dot_selected")
+	};
+};
 
 
-// Interaction au clic sur les Chevrons
-//
-//
-	arrowRight.addEventListener("click", ()=>{
-		const bulletpoint = document.querySelectorAll(".dot")
-		bulletpoint[index].classList.remove("dot_selected")
-		
-		index++
-		if (index >= slides.length -1) {
-			index = 0
-		}
-		bannerImg.setAttribute("src", slides[index].image)
-		bannerCaption.innerHTML = slides[index].tagLine
-		bulletpoint[index].classList.add("dot_selected")
-	})
+//Changements d'images, de texte, et de "dot" à appliquer
+// à l'execution de la fonction
+function carousel(){
+	////recupération des éléments "dot" du carousel
+	let dot = document.querySelectorAll(".dot");
+
+	////changement d'image et de texte
+	bannerImg.src = "assets/images/slideshow/"+slides[currentIndex].image;
+	bannerCaption.innerHTML = slides[currentIndex].tagLine;
+
+	////changement des bulletPoints à chaque changement du carousel
+	dot.forEach(function (dot, i){ 
+		dot.classList.toggle("dot_selected", i === currentIndex)
+	});
+	console.log(currentIndex);
+};
 
 
+// Interaction au clic sur les Chrevrons
 
-	arrowLeft.addEventListener("click", ()=>{
-		const bulletpoint = document.querySelectorAll(".dot")
-		bulletpoint[index].classList.remove("dot_selected")
-		
-		index--
-		if (index < 0) {
-			index = slides.length -1
-		}
-		bannerImg.setAttribute("src", slides[index].image)
-		bannerCaption.innerHTML = slides[index].tagLine
-		bulletpoint[index].classList.add("dot_selected")
-	})
+arrowRight.addEventListener("click", ()=>{
+	////incrémentation de l'index à chaque click : currentIndex === slides[index]
+	currentIndex = (currentIndex + 1 ) % slides.length;
+	carousel();
+});
+
+arrowLeft.addEventListener("click", ()=>{
+	currentIndex = (currentIndex -1 + slides.length) % slides.length;	
+	carousel();
+});
+
